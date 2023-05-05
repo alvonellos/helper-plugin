ebc95c3314f3492a960630c1de08d86513feeee0
GitHub Action <action@github.com>
2023-05-05 16:05:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/alvonellos/helperplugin/MyBundle.kt
+++ src/main/kotlin/com/github/alvonellos/helperplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.alvonellos.helperplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/alvonellos/helperplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/alvonellos/helperplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.alvonellos.helperplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
